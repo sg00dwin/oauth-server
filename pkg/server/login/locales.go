16400b3f7ab5f7cb58bd95f044f0ebfd3db9c58b
Jakub Hadvig <jhadvig@redhat.com>
2021-03-25 12:39:03 +0100

    CONSOLE-2535: Internationalize login page

---
+++ pkg/server/login/locales.go
@@ -0,0 +1,56 @@
+package login
+
+import "golang.org/x/text/language"
+
+func getLocale(langBase string) map[string]string {
+	switch langBase {
+	case language.English.String():
+		return locale_en
+	// Uncomment once given locale is available
+	// case language.Chinese.String():
+	// 	return locale_zh
+	// case language.Japanese.String():
+	// 	return locale_jp
+	// case language.Korean.String():
+	// 	return locale_ko
+	default:
+		return locale_en
+	}
+}
+
+func getPrefferedLang(acceptLangHeader string) string {
+	matcher := language.NewMatcher(supportedLangs)
+	userPrefs, _, err := language.ParseAcceptLanguage(acceptLangHeader)
+	if err != nil {
+		// if error occurs, fallback to English
+		return language.English.String()
+	}
+	tag, _, _ := matcher.Match(userPrefs...)
+	base, _ := tag.Base()
+	return base.String()
+}
+
+var supportedLangs = []language.Tag{
+	language.English, // en - first language is fallback
+	// Uncomment once given locale is available
+	// language.Chinese,  // zh
+	// language.Japanese, // jp
+	// language.Korean,   // ko
+}
+
+var locale_en = map[string]string{
+	"": "",
+}
+
+// Uncomment once given locale is available
+// var locale_zh = map[string]string{
+// 	"": "",
+// }
+
+// var locale_jp = map[string]string{
+// 	"": "",
+// }
+
+// var locale_ko = map[string]string{
+// 	"": "",
+// }
